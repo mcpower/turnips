f642286e1fd1fab1fe64793bf34ef0e85eb3d84f
mcpower <mcpower@users.noreply.github.com>
2020-04-07 21:56:14 +1000

    Save input to localstorage

--- js/calculator.js
+++ js/calculator.js
@@ -10,6 +10,18 @@ function getInputNum(id) {
         return out;
     }
 }
+function setInputNum(id, num) {
+    var elt = document.getElementById(id);
+    assert(elt !== null);
+    var out = num !== undefined ? num.toString() : "";
+    elt.value = out;
+}
+function toNull(x) {
+    return (x !== null && x !== void 0 ? x : null);
+}
+function fromNull(x) {
+    return (x !== null && x !== void 0 ? x : undefined);
+}
 function updateOutput() {
     console.log("updating!");
     var buy = getInputNum("buy");
@@ -20,8 +32,39 @@ function updateOutput() {
     var turnips = Turnips.fromInput(buy, sell);
     var output = turnips.generateAllPatterns().map(function (res) { return res.toString(); }).join("\n").replace(/,(?=\d)/g, ", ") || "No solutions found!";
     document.getElementById("calculator-output").innerText = output;
+    localStorage.setItem("input", JSON.stringify({ buy: toNull(buy), sell: sell.map(toNull) }));
+}
+function loadSavedInput() {
+    try {
+        var savedString = localStorage.getItem("input");
+        if (savedString === null) {
+            return;
+        }
+        var saved = JSON.parse(savedString);
+        if (typeof saved !== "object" || saved === null) {
+            return;
+        }
+        if (!("sell" in saved) || !("buy" in saved)) {
+            return;
+        }
+        var buy = saved.buy;
+        if (typeof buy === "number" || buy === null) {
+            setInputNum("buy", fromNull(buy));
+        }
+        var sell = saved.sell;
+        if (Array.isArray(sell)) {
+            sell.forEach(function (val, i) {
+                if (i < 12 && typeof val === "number" || val === null) {
+                    setInputNum("sell" + i, fromNull(val));
+                }
+            });
+        }
+    }
+    catch (e) {
+    }
 }
 window.addEventListener("load", function () {
     document.addEventListener("input", function () { return updateOutput(); });
+    loadSavedInput();
     updateOutput();
 });
--- js/calculator.ts
+++ js/calculator.ts
@@ -9,6 +9,23 @@ function getInputNum(id: string): number | undefined {
     }
 }
 
+function setInputNum(id: string, num: number | undefined) {
+    const elt = document.getElementById(id) as HTMLInputElement | null;
+    assert(elt !== null);
+    // NOTE: num.toString() could be in exponential form like 1e50
+    // we're gonna assume that we're not gonna pass in those values :P
+    const out = num !== undefined ? num.toString() : "";
+    elt.value = out;
+}
+
+function toNull<T>(x: T | undefined): T | null {
+    return x ?? null;
+}
+
+function fromNull<T>(x: T | null): T | undefined {
+    return x ?? undefined;
+}
+
 // NOTE: if calculating prices is expensive, we should probably
 // avoid recalculating everything on any input change
 function updateOutput() {
@@ -23,9 +40,46 @@ function updateOutput() {
     const turnips = Turnips.fromInput(buy, sell);
     const output = turnips.generateAllPatterns().map((res) => res.toString()).join("\n").replace(/,(?=\d)/g, ", ") || "No solutions found!";
     document.getElementById("calculator-output")!.innerText = output;
+
+    localStorage.setItem("input", JSON.stringify({buy: toNull(buy), sell: sell.map(toNull)}));
+}
+
+function loadSavedInput() {
+    try {
+        const savedString = localStorage.getItem("input");
+        if (savedString === null) {
+            return;
+        }
+        // this is safe, as we're in a try block
+        const saved = JSON.parse(savedString);
+        if (typeof saved !== "object" || saved === null) {
+            return;
+        }
+        // saved is guaranteed to be object-like.
+        // from now on, gracefully handle sell/buy errors.
+        if (!("sell" in saved) || !("buy" in saved)) {
+            return;
+        }
+        const buy = saved.buy;
+        if (typeof buy === "number" || buy === null) {
+            setInputNum("buy", fromNull(buy));
+        }
+        
+        const sell = saved.sell;
+        if (Array.isArray(sell)) {
+            sell.forEach((val, i) => {
+                if (i < 12 && typeof val === "number" || val === null) {
+                    setInputNum("sell" + i, fromNull(val));
+                }
+            });
+        }
+    } catch (e) {
+
+    }
 }
 
 window.addEventListener("load", () => {
     document.addEventListener("input", () => updateOutput());
+    loadSavedInput();
     updateOutput();
 });
